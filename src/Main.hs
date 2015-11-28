daf7002e6761b8b0c415e974163496df6d065010
Justin Leitgeb <justin@stackbuilders.com>
2015-11-28 08:40:18 -0500

    Use <> to append Texts

--- src/Main.hs
+++ src/Main.hs
@@ -10,7 +10,7 @@ import Network.HTTP.Types.Status (statusCode, Status(..))
 import System.Directory (getAppUserDataDirectory)
 import Data.Default (def)
 import Control.Monad (when, liftM)
-
+import Data.Monoid ((<>))
 
 import Turtle.Prelude
 import Turtle.Shell (using, sh, Shell(..), view)
@@ -77,16 +77,14 @@ getIfModifiedSince path outputFile url = do
     res <- HC.http req2 manager
     if statusCode (HC.responseStatus res) == notModified then
         do
-          echo $ T.concat
-                   [ "File '", filepathToText outputFile
-                   , "' already most recent version, using cached file." ]
+          echo $ "File '" <> filepathToText outputFile <>
+                 "' already most recent version, using cached file."
 
           return False
     else
         do
-          echo $ T.concat
-                   [ "File '", filepathToText outputFile
-                   , "' is outdated or missing, fetching most recent version." ]
+          echo $ "File '" <> filepathToText outputFile <>
+            "' is outdated or missing, fetching most recent version."
 
           HC.responseBody res C.$$+- sinkFile $ FPCOS.encodeString outputFile
 
@@ -138,8 +136,8 @@ installDependency statPath dj = do
       Just cmd -> shell cmd empty
       Nothing -> return ExitSuccess
 
-    echo $ T.concat [ "Moving ", filepathToText (mvSrc dj), " to "
-                    , filepathToText (mvDest dj)]
+    echo $ "Moving " <> filepathToText (mvSrc dj) <> " to " <>
+           filepathToText (mvDest dj)
 
     isDir <- testdir $ mvDest dj
 
@@ -180,35 +178,29 @@ main = sh $ do
                   , outputName = "mkgmap.zip"
                   , tmpPath = tpath
                   , mvSrc =
-                    FPCOS.fromText $ T.concat ["mkgmap-r", repr mkgmapRel]
+                    FPCOS.fromText $ "mkgmap-r" <> repr mkgmapRel
                   , mvDest = FP.append binPath "mkgmap"
-                  , sourceURL = T.concat
-                                [ "http://www.mkgmap.org.uk/download/mkgmap-r"
-                                , repr mkgmapRel, ".zip" ]
-                  , unpackCmd = Just (T.unwords ["unzip mkgmap.zip"]) }
+                  , sourceURL = "http://www.mkgmap.org.uk/download/mkgmap-r" <>
+                                repr mkgmapRel <> ".zip"
+                  , unpackCmd = Just "unzip mkgmap.zip" }
 
     , DownloadJob { jobName = "splitter"
                   , outputName = "splitter.zip"
                   , tmpPath = tpath
                   , mvSrc = FPCOS.fromText $
-                            T.concat ["splitter-r", repr splitterRel]
+                            "splitter-r" <> repr splitterRel
                   , mvDest = FP.append binPath "splitter"
-                  , sourceURL = T.concat
-                                [ "http://www.mkgmap.org.uk/download/splitter-r"
-                                , repr splitterRel
-                                , ".zip" ]
-                  , unpackCmd = Just (T.unwords ["unzip splitter.zip"]) }
+                  , sourceURL = "http://www.mkgmap.org.uk/download/splitter-r"
+                                <> repr splitterRel <> ".zip"
+                  , unpackCmd = Just "unzip splitter.zip" }
 
     , DownloadJob { jobName = "country"
                   , outputName = FPCOS.fromText countryFname
                   , tmpPath = tpath
                   , mvSrc = FPCOS.fromText $ countryFname
                   , mvDest = FP.append dataPath $ FPCOS.fromText $ countryFname
-                  , sourceURL = T.concat
-                                [ "http://download.geofabrik.de/"
-                                , region
-                                , "/"
-                                , countryFname ]
+                  , sourceURL = "http://download.geofabrik.de/" <>
+                                region <> "/" <> countryFname
                   , unpackCmd = Nothing }
 
     , DownloadJob { jobName = "bounds"
@@ -216,9 +208,8 @@ main = sh $ do
                   , tmpPath = tpath
                   , mvSrc = "bounds.zip"
                   , mvDest = FP.append dataPath "bounds.zip"
-                  , sourceURL = T.concat
-                                [ "http://osm2.pleiades.uni-wuppertal.de/"
-                                , "bounds/latest/bounds.zip" ]
+                  , sourceURL = "http://osm2.pleiades.uni-wuppertal.de/" <>
+                                "bounds/latest/bounds.zip"
                   , unpackCmd = Nothing }
 
     , DownloadJob { jobName = "sea"
@@ -226,9 +217,8 @@ main = sh $ do
                   , tmpPath = tpath
                   , mvSrc = "sea.zip"
                   , mvDest = FP.append dataPath "sea.zip"
-                  , sourceURL = T.concat
-                                [ "http://osm2.pleiades.uni-wuppertal.de/sea/"
-                                , "latest/sea.zip" ]
+                  , sourceURL = "http://osm2.pleiades.uni-wuppertal.de/sea/" <>
+                                "latest/sea.zip"
                   , unpackCmd = Nothing }
 
     , DownloadJob { jobName = "gmapi-builder"
@@ -236,9 +226,8 @@ main = sh $ do
                   , tmpPath = tpath
                   , mvSrc = "gmapi-builder/gmapi-builder.py"
                   , mvDest = FP.append binPath "gmapi-builder.py"
-                  , sourceURL = T.concat
-                                [ "http://bitbucket.org/berteun/gmapibuilder/"
-                                , "downloads/gmapi-builder.tar.gz" ]
+                  , sourceURL = "http://bitbucket.org/berteun/gmapibuilder/" <>
+                                "downloads/gmapi-builder.tar.gz"
                   , unpackCmd = Just "tar -xvzf gmapi-builder.tar.gz" }
     ]
 
@@ -247,46 +236,31 @@ main = sh $ do
   cd binPath
   splitOutputPath <- using (mktempdir tpath "split-output")
 
-  let splitterCmd = T.concat [ "java -jar splitter/splitter.jar --output-dir="
-                             , filepathToText splitOutputPath
-                             , " "
-                             , filepathToText dataPath
-                             , "/ecuador-latest.osm.pbf"]
+  let splitterCmd = "java -jar splitter/splitter.jar --output-dir=" <>
+                    filepathToText splitOutputPath <> " " <>
+                    filepathToText dataPath <> "/ecuador-latest.osm.pbf"
 
-  echo $ T.unwords ["splitter command:", splitterCmd]
+  echo $ "splitter command:" <> splitterCmd
 
   shell splitterCmd empty
 
   mkgmapOutputPath <- using (mktempdir tpath "mkgmap-output")
 
-  let mapName = T.concat [ "OSM ", country ]
+  let mapName = "OSM " <> country
 
-  let mkgmapCmd = T.concat [ "java -jar mkgmap/mkgmap.jar"
-                           , " --route"
-                           , " --add-pois-to-areas"
-                           , " --family-name=\"", mapName, "\""
-                           , " --series-name=\"", mapName, "\""
-                           , " --description=\"", mapName, "\""
-                           , " --mapname=55500001"
-                           , " --latin1"
-                           , " --precomp-sea="
-                           , filepathToText dataPath
-                           , "/sea.zip"
-                           , " --bounds="
-                           , filepathToText dataPath
-                           , "/bounds.zip"
-                           , " --index"
-                           , " --output-dir="
-                           , filepathToText mkgmapOutputPath
-                           , " --gmapsupp"
-                           , " "
-                           , filepathToText splitOutputPath
-                           , "/*.osm.pbf"
-                           ]
+  let mkgmapCmd =
+        "java -jar mkgmap/mkgmap.jar" <> " --route" <> " --add-pois-to-areas" <>
+        " --family-name=\"" <>  mapName <> "\"" <> " --series-name=\"" <>
+        mapName <> "\"" <> " --description=\"" <> mapName <> "\"" <>
+        " --mapname=55500001" <> " --latin1" <> " --precomp-sea=" <>
+        filepathToText dataPath <> "/sea.zip" <> " --bounds=" <>
+        filepathToText dataPath <> "/bounds.zip" <> " --index" <>
+        " --output-dir=" <> filepathToText mkgmapOutputPath <> " --gmapsupp " <>
+        filepathToText splitOutputPath <> "/*.osm.pbf"
 
   echo "Starting to make map..."
 
-  echo $ T.unwords ["mkgmap command:", mkgmapCmd]
+  echo $ "mkgmap command: " <> mkgmapCmd
 
   shell mkgmapCmd empty
 
@@ -300,35 +274,30 @@ main = sh $ do
   echo "Generated files from mkgmap:"
   view (ls mkgmapOutputPath)
 
-  let gmapCmd = T.concat [ "./gmapi-builder.py -t "
-                         , filepathToText mkgmapOutputPath
-                         , "/osmmap.tdb"
-                         , " -o "
-                         , filepathToText outputPath
-                         , " -b "
-                         , filepathToText mkgmapOutputPath
-                         , "/osmmap.img "
-                         , filepathToText mkgmapOutputPath
-                         , "/*.img"
-                         ]
+  let gmapCmd =
+        "./gmapi-builder.py -t " <> filepathToText mkgmapOutputPath <>
+        "/osmmap.tdb" <> " -o " <> filepathToText outputPath <>
+        " -b " <> filepathToText mkgmapOutputPath <> "/osmmap.img " <>
+        filepathToText mkgmapOutputPath <> "/*.img"
 
-  echo $ T.unwords ["gmapi-builder command:", gmapCmd]
+  echo $ "gmapi-builder command: " <> gmapCmd
 
   shell gmapCmd empty
 
-  echo $ T.unwords ["Output files copied to ", filepathToText outputPath]
+  echo $ "Output files copied to " <> filepathToText outputPath
 
   echo ""
   echo "Installation instructions:"
   echo ""
-  echo $ T.concat [ "Your completed maps have been placed in "
-                  , filepathToText outputPath, ":" ]
-  echo $ T.concat [ "Basecamp map (to copy to ~/Library/Application\\ "
-                  , "Support/Garmin/Maps/): ", filepathToText outputPath
-                  , "/", mapName, ".gmapi/", mapName, ".gmap/"]
+  echo $ "Your completed maps have been placed in " <>
+    filepathToText outputPath <> ":"
 
-  echo $ T.concat [ "The map for installation in your Garmin device: "
-                  , filepathToText outputPath, "/gmapsupp.img" ]
+  echo $ "Basecamp map (to copy to ~/Library/Application\\ " <>
+    "Support/Garmin/Maps/): " <> filepathToText outputPath <>
+    "/" <>  mapName <> ".gmapi/" <> mapName <> ".gmap/"
+
+  echo $ "The map for installation in your Garmin device: " <>
+    filepathToText outputPath <> "/gmapsupp.img"
 
   echo ""
 
